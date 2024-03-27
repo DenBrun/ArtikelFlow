2e89b5d346755e73b1cc32693e113d81be3aa9fe
Denys Brun <denchyk228@gmail.com>
2024-03-27 23:23:44 +0100

    Minor text changes

--- app/page.js
+++ app/page.js
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import Card from '../components/Card';
+import {Banner} from '../components/Banner';
+import { notFound } from 'next/navigation'
 
 
-export default async function Home() {  
-  const word = await fetch('http://127.0.0.1:8000/random_word', {cache: 'no-store'}).then(response => response.json())
+export default async function Home() { 
+  const word = await fetch('http://127.0.0.1:8000/random_word', {cache: 'no-store'})
+  .then(response => response.json())
+  .catch(error => {
+    return notFound()
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center lg:justify-between justify-center py-24 sm:px-20 px-5 dark dark:bg-black dark:text-white">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
+        <Banner/>        
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -31,7 +35,7 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[380px] sm:before:w-[480px] before:-translate-x-[10%] before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[180px] sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 after:dark:from-sky-900 after:dark:via-[#0141ff] before:lg:h-[360px] before:opacity-85 w-full justify-center">
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[380px] sm:before:w-[480px] before:-translate-x-[10%] before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[180px] sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 after:dark:from-sky-900 after:dark:via-[#0141ff] before:lg:h-[360px] before:opacity-55 w-full justify-center">
         <Card word={word} />
       </div>
 
---
+++ components/Banner.js
@@ -0,0 +1,27 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export const Banner = () => {
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    useEffect(() => {
+        function handleResize() {
+        setIsSmallScreen(window.innerWidth < 640);
+        }
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        // Remove event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return (
+    <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          {isSmallScreen ? 'Placeholder text' : (
+            <>Tip: Use keys&nbsp;
+              <span className="font-mono font-bold">[1, 2, 3]</span>
+              &nbsp;for faster selection
+            </>
+          )}
+    </p>
+  )
+}
