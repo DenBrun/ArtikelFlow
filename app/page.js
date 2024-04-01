7d4549ebb833e8522d3e9ce642194391eb932e14
Denys Brun <denchyk228@gmail.com>
2024-04-01 23:45:25 +0200

    Changed icons

--- app/page.js
+++ app/page.js
@@ -18,28 +18,24 @@ export default async function Home() {
     <main className="flex min-h-screen md:flex-col flex-col-reverse lg:items-center lg:justify-between justify-center py-24 sm:px-20 px-5 dark dark:bg-black dark:text-white">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Banner/>        
-        <div className="fixed bottom-0 left-0 flex h-32 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
+        <div className="fixed bottom-0 left-0 flex h-28 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <div
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
           >
-            By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src="/ArtikelFlow.svg"
+              alt="ArtikelFlow Logo"
+              className="w-[180px] h-auto"
+              width={0}
+              height={0}
               priority
             />
-          </a>
+          </div>
         </div>
       </div>
       <ViewPreferenceProvider>
         <LevelProvider>
-          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[380px] sm:before:w-[480px] before:-translate-x-[10%] before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[180px] sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 after:dark:from-sky-900 after:dark:via-[#0141ff] before:lg:h-[360px] before:opacity-55 w-full justify-center flex-1 sm:m-0 mt-20" >
+          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[380px] sm:before:w-[480px] before:-translate-x-[10%] before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[180px] sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 after:dark:from-sky-900 after:dark:via-[#0141ff] before:lg:h-[360px] before:opacity-55 w-full justify-center flex-1 sm:m-0 mt-5" >
             <Card word={word} />
           </div>
       
